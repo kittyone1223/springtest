85e7e9c6a380c0f4e667f1ec14de5e0d8c1f7083
wx <helloktwx@163.com>
2020-11-26 23:53:30 +0800

    新建test.java

---
+++ src/main/java/com/offcn/Test.java
@@ -0,0 +1,12 @@
+package com.offcn;
+
+/**
+ * @program: springtest
+ * @author: wangxin
+ * @create: 2020-11-26 23:52
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
